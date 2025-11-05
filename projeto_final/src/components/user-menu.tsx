dcde771fabc774bde0ece5c442eca76b153a2509
Pincinato07 <jgpincinato@gmail.com>
2025-11-05 13:02:17 -0300

    feat: adiciona layout do painel com sidebar e header

---
+++ projeto_final/src/components/user-menu.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { User } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function UserMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="sm" className="gap-2">
+          <User className="h-5 w-5" />
+          <span className="hidden sm:inline">Conta</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-48">
+        <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Perfil</DropdownMenuItem>
+        <DropdownMenuItem>Configurações</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Sair</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
